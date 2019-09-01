3447265ba1bb121ca114525b37806c5a9d031713
shen-suan <00winner99@gmail.com>
2019-09-01 13:23:06 +0800

    AR更新

--- misbookstore/app/src/main/java/com/example/bookstore/arContent/augmentedimage/AugmentedImageNode.java
+++ misbookstore/app/src/main/java/com/example/bookstore/arContent/augmentedimage/AugmentedImageNode.java
@@ -65,22 +65,6 @@ public class AugmentedImageNode extends AnchorNode {
   public AugmentedImageNode(Context context) {
     // Upon construction, start loading the models for the corners of the frame.
     if (view == null) {
-//      ulCorner =
-//          ModelRenderable.builder()
-//              .setSource(context, Uri.parse("models/frame_upper_left.sfb"))
-//              .build();
-//      urCorner =
-//          ModelRenderable.builder()
-//              .setSource(context, Uri.parse("models/frame_upper_right.sfb"))
-//              .build();
-//      llCorner =
-//          ModelRenderable.builder()
-//              .setSource(context, Uri.parse("models/frame_lower_left.sfb"))
-//              .build();
-//      lrCorner =
-//          ModelRenderable.builder()
-//              .setSource(context, Uri.parse("models/frame_lower_right.sfb"))
-//              .build();
       view = ViewRenderable.builder()
               .setView(context, R.layout.ar_book_info)
               .build();
@@ -100,17 +84,6 @@ public class AugmentedImageNode extends AnchorNode {
   @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
   public void setImage(AugmentedImage image) {
     this.image = image;
-
-    // If any of the models are not loaded, then recurse when all are loaded.
-//    if (!ulCorner.isDone() || !urCorner.isDone() || !llCorner.isDone() || !lrCorner.isDone()) {
-//      CompletableFuture.allOf(ulCorner, urCorner, llCorner, lrCorner)
-//          .thenAccept((Void aVoid) -> setImage(image))
-//          .exceptionally(
-//              throwable -> {
-//                Log.e(TAG, "Exception loading", throwable);
-//                return null;
-//              });
-//    }
 
     // Set the anchor based on the center of the image.
     setAnchor(image.createAnchor(image.getCenterPose()));
@@ -137,7 +110,7 @@ public class AugmentedImageNode extends AnchorNode {
       //cornerNode = new Node();
       cornerNode.setParent(this);
       cornerNode.setLocalPosition(localPosition);
-      cornerNode.setRenderable(urCorner.getNow(null));
+      cornerNode.setRenderable(view.getNow(null));
 
       localPosition.set(-0.2f * image.getExtentX(), 0.0f, 0.5f * image.getExtentZ());
       //cornerNode = new Node();
