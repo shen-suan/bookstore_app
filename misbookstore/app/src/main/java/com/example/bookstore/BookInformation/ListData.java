5b545eff22f0eff929ab0c9c7a4fecdc733c5d2c
Serena9838 <s2900153@gmail.com>
2019-09-02 17:59:41 +0800

    //listdata

--- misbookstore/app/src/main/java/com/example/bookstore/BookInformation/ListData.java
+++ misbookstore/app/src/main/java/com/example/bookstore/BookInformation/ListData.java
@@ -1,9 +1,18 @@
 package com.example.bookstore.BookInformation;
 
 public class ListData {
-    private final String title;
-    private final int price;
-    private final String isbn;
+//    private final String title;
+//    private final int price;
+//    private final String isbn;
+//
+    public String title;
+    public int price;
+    public String isbn;
+
+
+    public ListData() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
 
     public ListData(String title,int price,String isbn) {
         this.title = title;
